1b0711364373cf61bc5f21e26ed9c39e1bc64530
Marcelo Salhab Brogliato <msbrogli@gmail.com>
2018-09-12 10:33:27 +0000

    feat(manager): Initialize manager's components (tx_storage and wallet).

--- tests/p2p/test_protocol.py
+++ tests/p2p/test_protocol.py
@@ -1,11 +1,11 @@
-from twisted.internet import reactor
-from twisted.trial import unittest
 from twisted.test import proto_helpers
 from twisted.python import log
 
 from hathor.p2p.peer_id import PeerId
 from hathor.p2p.factory import HathorServerFactory, HathorClientFactory
 from hathor.p2p.manager import HathorManager
+
+from tests import unittest
 
 import sys
 
@@ -35,46 +35,6 @@ class HathorProtocolTestCase(unittest.TestCase):
 
     def tearDown(self):
         self.clean_pending(required_to_quiesce=False)
-
-    def clean_pending(self, required_to_quiesce=True):
-        """
-        This handy method cleans all pending tasks from the reactor.
-
-        When writing a unit test, consider the following question:
-
-            Is the code that you are testing required to release control once it
-            has done its job, so that it is impossible for it to later come around
-            (with a delayed reactor task) and do anything further?
-
-        If so, then trial will usefully test that for you -- if the code under
-        test leaves any pending tasks on the reactor then trial will fail it.
-
-        On the other hand, some code is *not* required to release control -- some
-        code is allowed to continuously maintain control by rescheduling reactor
-        tasks in order to do ongoing work.  Trial will incorrectly require that
-        code to clean up all its tasks from the reactor.
-
-        Most people think that such code should be amended to have an optional
-        "shutdown" operation that releases all control, but on the contrary it is
-        good design for some code to *not* have a shutdown operation, but instead
-        to have a "crash-only" design in which it recovers from crash on startup.
-
-        If the code under test is of the "long-running" kind, which is *not*
-        required to shutdown cleanly in order to pass tests, then you can simply
-        call testutil.clean_pending() at the end of the unit test, and trial will
-        be satisfied.
-
-        Copy from: https://github.com/zooko/pyutil/blob/master/pyutil/testutil.py#L68
-        """
-        pending = reactor.getDelayedCalls()
-        active = bool(pending)
-        for p in pending:
-            if p.active():
-                p.cancel()
-            else:
-                print('WEIRDNESS! pending timed call not active!')
-        if required_to_quiesce and active:
-            self.fail('Reactor was still active when it was required to be quiescent.')
 
     def _send_cmd(self, proto, cmd, payload=None):
         if not payload:
