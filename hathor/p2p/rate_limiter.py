213b5d7a2f590fac0e95a013dc533b9d4f8b5ed3
Pedro Ferreira <phsf.pedro@gmail.com>
2018-10-05 04:03:25 +0000

    feat(ws): control limit of exchanged messages in websocket

--- hathor/p2p/rate_limiter.py
+++ hathor/p2p/rate_limiter.py
@@ -11,24 +11,42 @@ class RateLimiter(object):
     """
 
     def __init__(self):
+        # Stores the keys that are being limited and it's RateLimit
+        # Dict[string, RateLimiterLimit]
         self.keys = {}
+
+        # Stores the last hit for each key
+        # Dict[string, RateLimiterLimit]
         self.hits = {}
 
     def set_limit(self, key, max_hits, window_seconds):
         """ Set a limit to a given key, e.g., `max_hits = 10` and
         `window_seconds = 60` means at most 10 hits per minute.
+
+        :param key: Name of key to set the rate limit
+        :type key: string
+
+        :param max_hits: Maximum hits allowed for this key before the limit
+        :type max_hits: int
+
+        :param window_seconds: The maximum hits can be done in window_seconds
+        :type window_seconds: int
         """
         assert(window_seconds > 0)
         self.keys[key] = RateLimiterLimit(max_hits, window_seconds)
 
     def get_limit(self, key):
         """ Get a limit to a given key.
+
+        :type key: string
         """
         return self.keys.get(key, None)
 
     def unset_limit(self, key):
         """ Unset a limit to a given key. Next calls to `add_hit` with that key
         will be ignored.
+
+        :type key: string
         """
         self.reset(key)
         self.keys.pop(key, None)
@@ -36,6 +54,11 @@ class RateLimiter(object):
     def add_hit(self, key, weight=1):
         """ Add a hit to a given key. You can use `weight` to add more than one hit.
         Return `True` if threshold has not been reached, or `False` otherwise.
+
+        :type key: string
+
+        :param weight: How many hits this 'hit' means
+        :type weight: int
         """
         if key not in self.keys:
             return True
@@ -75,5 +98,7 @@ class RateLimiter(object):
 
     def reset(self, key):
         """ Reset the hits of a given key.
+
+            :type key: string
         """
         self.hits.pop(key, None)
