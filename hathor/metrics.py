8a6b98c487d7578c9dfa72014b2f795f7aa11f6b
Pedro Ferreira <phsf.pedro@gmail.com>
2018-10-11 11:50:04 -0300

    feat: get metric history

--- hathor/metrics.py
+++ hathor/metrics.py
@@ -1,17 +1,25 @@
 from hathor.pubsub import PubSubManager, HathorEvents
+from hathor.transaction.storage.memory_storage import TransactionMemoryStorage
 
 
 class Metrics:
-    def __init__(self, pubsub=None):
+    def __init__(self, pubsub=None, tx_storage=None):
         self.transactions = 0
         self.blocks = 0
 
         self.pubsub = pubsub or PubSubManager()
 
+        self.tx_storage = tx_storage or TransactionMemoryStorage()
+
         self._initial_setup()
 
     def _initial_setup(self):
+        self._start_initial_values()
         self.subscribe()
+
+    def _start_initial_values(self):
+        self.transactions = self.tx_storage.get_tx_count()
+        self.blocks = self.tx_storage.get_block_count()
 
     def subscribe(self):
         """ Subscribe to defined events for the pubsub received
