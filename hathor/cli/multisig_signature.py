8019d9bfc6784e4627427375fe52aed357d0bc96
Pedro Ferreira <phsf.pedro@gmail.com>
2018-12-16 19:53:10 +0000

    tests: testing cli of signature

--- hathor/cli/multisig_signature.py
+++ hathor/cli/multisig_signature.py
@@ -4,16 +4,22 @@ from hathor.transaction import Transaction
 from hathor.wallet.util import generate_signature
 
 
-def main():
+def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('partial_tx', type=str, help='Tx to be signed in hex')
     parser.add_argument('private_key', type=str, help='Encrypted private key in hex')
+    return parser
 
-    args = parser.parse_args()
 
-    priv_key_password = getpass.getpass(prompt='Password to decrypt the private key:')
-
+def execute(args, priv_key_password):
     tx = Transaction.create_from_struct(bytes.fromhex(args.partial_tx))
 
     signature = generate_signature(tx, bytes.fromhex(args.private_key), password=priv_key_password.encode())
     print('Signature: ', signature.hex())
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+    priv_key_password = getpass.getpass(prompt='Password to decrypt the private key:')
+    execute(args, priv_key_password)
