9258c253029d6b96f5fdc0d734b85e4b86d2583d
Pedro Ferreira <phsf.pedro@gmail.com>
2018-12-16 19:23:50 +0000

    tests: testing wallet cli

---
+++ tests/cli/test_wallet.py
@@ -0,0 +1,21 @@
+from tests import unittest
+from hathor.cli.wallet import create_parser, execute
+import tempfile
+import json
+
+
+class WalletTest(unittest.TestCase):
+    def test_wallet(self):
+        parser = create_parser()
+
+        tmpdir = tempfile.mkdtemp()
+        print(tmpdir)
+
+        count = 5
+        args = parser.parse_args(['--count', '{}'.format(count), '--directory', tmpdir])
+        execute(args, '1234')
+
+        with open('{}/keys.json'.format(tmpdir), 'rb') as f:
+            data = json.loads(f.read())
+
+        self.assertEqual(len(data), 5)
