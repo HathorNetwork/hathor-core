43325d460b985f72e1d662a50f996e08c6be45e9
Marcelo Salhab Brogliato <msbrogli@gmail.com>
2019-03-26 23:25:39 -0300

    fix(logging): Minor errors in LogRateLimiter

--- hathor/logging.py
+++ hathor/logging.py
@@ -1,18 +1,21 @@
 from collections import defaultdict
-from typing import Dict
+from typing import TYPE_CHECKING, Dict
 
 from hathor.p2p.rate_limiter import RateLimiter
 
+if TYPE_CHECKING:
+    from hathor.manager import HathorManager  # noqa: F401
 
-class LogRateLimiter(RateLimiter):
-    def __init__(self, manager):
+
+class LogRateLimiter:
+    def __init__(self, manager: 'HathorManager'):
         self.manager = manager
 
         self.rate_limiter = RateLimiter()
         self.suppression_message: Dict[str, str] = {}
 
         self.counters: Dict[str, int] = defaultdict(int)
-        self.latest_log: Dict[str, int] = defaultdict(int)
+        self.latest_log: Dict[str, float] = defaultdict(int)
 
         # Delay between message suppression logs.
         self.suppression_log_delay: int = 5
@@ -24,31 +27,32 @@ class LogRateLimiter(RateLimiter):
         self.rate_limiter.set_limit(key, max_hits, window_seconds)
         self.suppression_message[key] = suppression_message
 
-    def info(self, key: str, *args, **kwargs):
+    def info(self, key: str, *args, **kwargs) -> None:
         if self.rate_limiter.add_hit(key):
-            if self.counters[key] > 0:
-                self.do_suppression_log()
+            if self.delayed_call and self.delayed_call.active:
+                self.delayed_call.cancel()
+                self.do_suppression_log(key)
             self.manager.log.info(*args, **kwargs)
         else:
             if self.counters[key] == 0:
-                self.schedule_suppression_log()
+                self.schedule_suppression_log(key)
             self.counters[key] += 1
 
-    def schedule_suppression_log(self):
+    def schedule_suppression_log(self, key: str) -> None:
         if self.delayed_call and self.delayed_call.active:
             return
-        self.delayed_call = self.manager.reactor.callLater(self.suppression_log_delay, self.do_suppression_log)
+        self.latest_log[key] = self.get_seconds()
+        self.delayed_call = self.manager.reactor.callLater(self.suppression_log_delay, self.do_suppression_log, key)
 
-    def get_seconds(self):
+    def get_seconds(self) -> float:
         return self.manager.reactor.seconds()
 
-    def do_suppression_log(self, key: str):
+    def do_suppression_log(self, key: str) -> None:
         counter = self.counters[key]
         msg = self.suppression_message[key]
         now = self.get_seconds()
         dt = now - self.latest_log[key]
 
-        self.log.info(msg, counter=counter, dt=dt)
+        self.manager.log.info(msg, counter=counter, dt=dt)
 
         self.counters[key] = 0
-        self.latest_log[key] = now
