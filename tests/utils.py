95507a7e214420bd620464bebf0f36e613136413
Marcelo Salhab Brogliato <msbrogli@gmail.com>
2019-12-23 20:25:34 +0000

    fix(tx): Wrongly calculating the accumulated weight when a block indirectly verifies the transaction

--- tests/utils.py
+++ tests/utils.py
@@ -145,7 +145,7 @@ def add_new_transactions(manager, num_txs, advance_clock=None):
     return txs
 
 
-def add_new_block(manager, advance_clock=None, *, parent_block_hash=None, data=b''):
+def add_new_block(manager, advance_clock=None, *, parent_block_hash=None, data=b'', weight=None):
     """ Create, resolve and propagate a new block
 
         :param manager: Manager object to handle the creation
@@ -155,6 +155,8 @@ def add_new_block(manager, advance_clock=None, *, parent_block_hash=None, data=b
         :rtype: :py:class:`hathor.transaction.block.Block`
     """
     block = manager.generate_mining_block(parent_block_hash=parent_block_hash, data=data)
+    if weight is not None:
+        block.weight = weight
     block.resolve()
     block.verify()
     manager.propagate_tx(block, fails_silently=False)
@@ -163,7 +165,7 @@ def add_new_block(manager, advance_clock=None, *, parent_block_hash=None, data=b
     return block
 
 
-def add_new_blocks(manager, num_blocks, advance_clock=None, *, parent_block_hash=None, block_data=b''):
+def add_new_blocks(manager, num_blocks, advance_clock=None, *, parent_block_hash=None, block_data=b'', weight=None):
     """ Create, resolve and propagate some blocks
 
         :param manager: Manager object to handle the creation
@@ -177,7 +179,9 @@ def add_new_blocks(manager, num_blocks, advance_clock=None, *, parent_block_hash
     """
     blocks = []
     for _ in range(num_blocks):
-        blocks.append(add_new_block(manager, advance_clock, parent_block_hash=parent_block_hash, data=block_data))
+        blocks.append(
+            add_new_block(manager, advance_clock, parent_block_hash=parent_block_hash, data=block_data, weight=weight)
+        )
         if parent_block_hash:
             parent_block_hash = blocks[-1].hash
     return blocks
