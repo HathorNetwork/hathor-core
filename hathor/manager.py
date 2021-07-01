7f8d8b6d9005fec5f36a076c01bfe3cbcb4b3048
Marcelo Salhab Brogliato <msbrogli@gmail.com>
2021-06-30 22:43:32 -0700

    fix(init): Remove voided tx tips from indexes
    This fixes a regression caused by PR #226.
    
    Co-authored-by: Jan Segre <jan@hathor.network>

--- hathor/manager.py
+++ hathor/manager.py
@@ -408,6 +408,8 @@ class HathorManager:
                     elif tx.is_block:
                         self.tx_storage.add_to_parent_blocks_index(tx.hash)
                     self.tx_storage.add_to_indexes(tx)
+                    if tx.is_transaction and tx_meta.voided_by:
+                        self.tx_storage.del_from_indexes(tx)
             except (InvalidNewTransaction, TxValidationError):
                 self.log.error('unexpected error when initializing', tx=tx, exc_info=True)
                 raise
