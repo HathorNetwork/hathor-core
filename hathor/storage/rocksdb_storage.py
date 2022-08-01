ae8e678aa8a336225134dbb2803d72444f27d00c
lucas3003 <lucas_5639@hotmail.com>
2022-08-01 12:36:12 -0300

    fix imports

--- hathor/storage/rocksdb_storage.py
+++ hathor/storage/rocksdb_storage.py
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os, rocksdb
+import os
 from typing import List, Optional
 
+import rocksdb
 from structlog import get_logger
 
 logger = get_logger()
