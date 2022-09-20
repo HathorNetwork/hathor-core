7ad61f86d7d3992f511f1e1675548de196b0be48
Luis Helder <luislhl@gmail.com>
2022-09-20 16:46:37 -0300

    style: fix linters

--- hathor/metrics.py
+++ hathor/metrics.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
 from collections import deque
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Deque, Dict, List, NamedTuple, Optional
