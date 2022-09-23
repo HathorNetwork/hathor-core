c292bf2996d3fe01a3b0d6cce4583fe51373a961
Luis Helder <luislhl@gmail.com>
2022-09-23 09:59:07 -0300

    feat: log the network name as well

--- hathor/util.py
+++ hathor/util.py
@@ -772,6 +772,7 @@ class EnvironmentInfo:
     hathor_core_args: str
     hathor_core_version: str
     peer_id: str
+    network: str
 
     def as_dict(self):
         return asdict(self)
@@ -781,7 +782,8 @@ def get_environment_info(args: Namespace, peer_id: str) -> EnvironmentInfo:
         python_implementation=str(sys.implementation),
         hathor_core_args=str(args),
         hathor_core_version=get_hathor_core_version(),
-        peer_id=peer_id
+        peer_id=peer_id,
+        network=settings.NETWORK_NAME
     )
 
     return environment_info
