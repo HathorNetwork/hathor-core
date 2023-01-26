6f2a3ef434171478b34ff9304b516a8ed25c433a
Gabriel Levcovitz <g.levcovitz@gmail.com>
2023-01-26 01:04:55 -0300

    feat(events): refactor EventManager

--- hathor/event/event_manager.py
+++ hathor/event/event_manager.py
@@ -88,23 +88,12 @@ _EVENT_EXTRACT_MAP: Dict[HathorEvents, Callable[[EventArguments], Dict[str, Any]
 }
 
 
-def _build_event_data(event: HathorEvents, event_args: EventArguments) -> Dict[str, Any]:
+def _build_event_data(event_type: HathorEvents, event_args: EventArguments) -> Dict[str, Any]:
     """Extract and build event data from event_args for a given event type."""
-    event_extract_fn = _EVENT_EXTRACT_MAP.get(event)
+    event_extract_fn = _EVENT_EXTRACT_MAP.get(event_type)
     if event_extract_fn is None:
-        raise ValueError(f'The given event type ({event}) is not a supported event')
+        raise ValueError(f'The given event type ({event_type}) is not a supported event')
     return event_extract_fn(event_args)
-
-
-def _assert_unclosed_event_group(last_event: BaseEvent):
-    last_event_type = HathorEvents(last_event.type) if last_event is not None else None
-    # XXX: we must check that the last event either does not belong to an event group or that it just closed an
-    #      event group, because we cannot resume an open group of events that wasn't properly closed before exit
-    assert (
-        last_event is None or
-        last_event.group_id is None or
-        last_event_type in _GROUP_END_EVENTS
-    ), 'an unclosed event group was detected, which indicates the node crashed, cannot resume'
 
 
 class EventManager:
@@ -112,62 +101,103 @@ class EventManager:
         self,
         event_storage: EventStorage,
         event_ws_factory: EventWebsocketFactory,
+        pubsub: PubSubManager,
         reactor: Reactor,
         peer_id: str
     ):
         self.log = logger.new()
-        self.clock = reactor
-        self.event_storage = event_storage
-        self.event_ws_factory = event_ws_factory
 
-        last_event = self.event_storage.get_last_event()
-        last_group_id = self.event_storage.get_last_group_id()
-
-        _assert_unclosed_event_group(last_event)
-
-        self._next_event_id = 0 if last_event is None else last_event.id + 1
-        self._next_group_id = 0 if last_group_id is None else last_group_id + 1
-        self._current_group_id: Optional[int] = None
+        self._clock = reactor
+        self._event_storage = event_storage
+        self._event_ws_factory = event_ws_factory
+        self._pubsub = pubsub
         self._peer_id = peer_id
 
-    def subscribe(self, pubsub: PubSubManager) -> None:
+        self._last_event = self._event_storage.get_last_event()
+        self._last_existing_group_id = self._event_storage.get_last_group_id()
+
+        self._assert_closed_event_group()
+        self._subscribe_events()
+
+    def _assert_closed_event_group(self):
+        # XXX: we must check that the last event either does not belong to an event group or that it just closed an
+        #      event group, because we cannot resume an open group of events that wasn't properly closed before exit
+        assert (
+            self._event_group_is_closed()
+        ), 'an unclosed event group was detected, which indicates the node crashed, cannot resume'
+
+    def _event_group_is_closed(self):
+        return (
+            self._last_event is None or
+            self._last_event.group_id is None or
+            HathorEvents(self._last_event.type) in _GROUP_END_EVENTS
+        )
+
+    def _subscribe_events(self):
         """ Subscribe to defined events for the pubsub received
         """
         for event in _SUBSCRIBE_EVENTS:
-            pubsub.subscribe(event, self._handle_event)
+            self._pubsub.subscribe(event, self._handle_event)
 
-    def _handle_event(self, event: HathorEvents, event_args: EventArguments) -> None:
-        event_id = self._next_event_id
-        group_id = self._current_group_id
+    def _handle_event(self, event_type: HathorEvents, event_args: EventArguments) -> None:
+        create_event_fn: Callable[[HathorEvents, EventArguments], BaseEvent]
 
-        if event in _GROUP_START_EVENTS:
-            assert self._current_group_id is None, 'A new event group cannot be started as one is already in progress.'
-            group_id = self._next_group_id
+        if event_type in _GROUP_START_EVENTS:
+            create_event_fn = self._create_group_start_event
+        elif event_type in _GROUP_END_EVENTS:
+            create_event_fn = self._create_group_end_event
+        else:
+            create_event_fn = self._create_non_group_edge_event
 
-        elif event in _GROUP_END_EVENTS:
-            assert self._current_group_id is not None, 'Cannot end event group as none is in progress.'
-            self._current_group_id = None
+        event = create_event_fn(event_type, event_args)
 
-        base_event = BaseEvent(
-            id=event_id,
-            peer_id=self._peer_id,
-            timestamp=self.clock.seconds(),
-            type=event.value,
-            data=_build_event_data(event, event_args),
+        self._event_storage.save_event(event)
+        self._event_ws_factory.broadcast_event(event)
+
+        self._last_event = event
+
+    def _create_group_start_event(self, event_type: HathorEvents, event_args: EventArguments) -> BaseEvent:
+        assert self._event_group_is_closed(), 'A new event group cannot be started as one is already in progress.'
+
+        new_group_id = 0 if self._last_existing_group_id is None else self._last_existing_group_id + 1
+
+        self._last_existing_group_id = new_group_id
+
+        return self._create_event(
+            event_type=event_type,
+            event_args=event_args,
+            group_id=new_group_id,
+        )
+
+    def _create_group_end_event(self, event_type: HathorEvents, event_args: EventArguments) -> BaseEvent:
+        assert not self._event_group_is_closed(), 'Cannot end event group as none is in progress.'
+
+        return self._create_event(
+            event_type=event_type,
+            event_args=event_args,
+            group_id=self._last_event.group_id,
+        )
+
+    def _create_non_group_edge_event(self, event_type: HathorEvents, event_args: EventArguments) -> BaseEvent:
+        group_id = None if self._event_group_is_closed() else self._last_event.group_id
+
+        return self._create_event(
+            event_type=event_type,
+            event_args=event_args,
             group_id=group_id,
         )
 
-        self._persist_event(base_event)
-        self._broadcast_event(base_event)
-
-        self._next_event_id += 1
-
-        if event in _GROUP_START_EVENTS:
-            self._current_group_id = self._next_group_id
-            self._next_group_id += 1
-
-    def _persist_event(self, event: BaseEvent) -> None:
-        self.event_storage.save_event(event)
-
-    def _broadcast_event(self, event: BaseEvent) -> None:
-        self.event_ws_factory.broadcast_event(event)
+    def _create_event(
+        self,
+        event_type: HathorEvents,
+        event_args: EventArguments,
+        group_id: Optional[int],
+    ) -> BaseEvent:
+        return BaseEvent(
+            id=0 if self._last_event is None else self._last_event.id + 1,
+            peer_id=self._peer_id,
+            timestamp=self._clock.seconds(),
+            type=event_type.value,
+            data=_build_event_data(event_type, event_args),
+            group_id=group_id,
+        )
