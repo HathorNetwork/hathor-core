265cf37958a84a1675f66afe5acb9c1e55853ca6
Gabriel Levcovitz <g.levcovitz@gmail.com>
2023-02-03 21:05:56 -0300

    feat(events): change test utils so filepath is not hardcoded

--- tests/unittest.py
+++ tests/unittest.py
@@ -32,8 +32,14 @@ def shorten_hash(container):
     return container_type(h[-2:].hex() for h in container)
 
 
-def _load_peer_id_pool(file_path: str = 'tests/peer_id_pool.json') -> Iterator[PeerId]:
+def _load_peer_id_pool(file_path: Optional[str] = None) -> Iterator[PeerId]:
     import json
+
+    if file_path is None:
+        this_file_path = os.path.dirname(__file__)
+        file_name = 'peer_id_pool.json'
+        file_path = os.path.join(this_file_path, file_name)
+
     with open(file_path) as peer_id_pool_file:
         peer_id_pool_dict = json.load(peer_id_pool_file)
         for peer_id_dict in peer_id_pool_dict:
