9b5e435901347aa034d1e3ad9363ec4079135b52
Gabriel Levcovitz <g.levcovitz@gmail.com>
2023-02-27 16:56:11 -0300

    feat(events): change event data dict to classes

--- hathor/event/base_event.py
+++ hathor/event/base_event.py
@@ -12,14 +12,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict, Optional
+from typing import Optional, Union
 
 from pydantic import NonNegativeInt
 
+from hathor.pubsub import EventArguments, HathorEvents
 from hathor.utils.pydantic import BaseModel
 
 
-class BaseEvent(BaseModel):
+class BaseEventData(BaseModel):
+    @classmethod
+    def from_event_arguments(cls, args: EventArguments) -> 'EventData':
+        raise NotImplementedError()
+
+
+class EmptyData(BaseEventData):
+    @classmethod
+    def from_event_arguments(cls, args: EventArguments) -> 'EmptyData':
+        return EmptyData()
+
+
+class TxData(BaseEventData):
+    hash: str
+    # TODO: Other fields
+
+    @classmethod
+    def from_event_arguments(cls, args: EventArguments) -> 'TxData':
+        return TxData(
+            hash=args.tx.hash_hex,
+        )
+
+
+class ReorgData(BaseEventData):
+    reorg_size: int
+    previous_best_block: str
+    new_best_block: str
+    common_block: str
+
+    @classmethod
+    def from_event_arguments(cls, args: EventArguments) -> 'ReorgData':
+        return ReorgData(
+            reorg_size=args.reorg_size,
+            previous_best_block=args.old_best_block.hash_hex,
+            new_best_block=args.new_best_block.hash_hex,
+            common_block=args.common_block.hash_hex,
+        )
+
+
+EventData = Union[EmptyData, TxData, ReorgData]
+
+
+class BaseEvent(BaseModel, use_enum_values=True):
     # Full node id, because different full nodes can have different sequences of events
     peer_id: str
     # Event unique id, determines event order
@@ -29,9 +72,10 @@ class BaseEvent(BaseModel):
     # events it's possible that timestamps will temporarily decrease.
     timestamp: float
     # One of the event types
-    type: str  # TODO: Convert type and data to enum and classes
+    type: HathorEvents
     # Variable for event type
-    data: Dict
+    data: EventData
     # Used to link events, for example, many TX_METADATA_CHANGED will have the same group_id when they belong to the
     # same reorg process
     group_id: Optional[NonNegativeInt] = None
+    # TODO: Custom validate that EventData is equivalent to HathorEvents
