8f32d433140ead1ab0eed8376cbcd67fa9ff3d33
Gabriel Levcovitz <g.levcovitz@gmail.com>
2023-04-18 17:03:30 -0300

    feat(events): lint code

--- hathor/event/model/node_state.py
+++ hathor/event/model/node_state.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from enum import Enum, auto
+from enum import Enum
 
 
 class NodeState(Enum):
