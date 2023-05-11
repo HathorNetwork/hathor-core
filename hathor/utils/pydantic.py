677d2f35e11ce7790030c6cd67006f8c6d3c7f06
Gabriel Levcovitz <g.levcovitz@gmail.com>
2023-05-11 04:23:30 -0300

    refactor: implement settings migration tests and fix parsing

--- hathor/utils/pydantic.py
+++ hathor/utils/pydantic.py
@@ -12,10 +12,14 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from typing import Union, TypeVar
+
+import yaml
 from pydantic import BaseModel as PydanticBaseModel, Extra
 from pydantic.generics import GenericModel as PydanticGenericModel
 
-from hathor.util import json_dumpb
+
+T = TypeVar('T', bound='BaseModel')
 
 
 class BaseModel(PydanticBaseModel):
@@ -28,7 +32,15 @@ class BaseModel(PydanticBaseModel):
 
     def json_dumpb(self) -> bytes:
         """Utility method for converting a Model into bytes representation of a JSON."""
+        from hathor.util import json_dumpb
         return json_dumpb(self.dict())
+
+    @classmethod
+    def from_yaml(cls, *, filepath: str) -> T:
+        with open(filepath, 'r') as file:
+            obj = yaml.safe_load(file)
+
+            return cls(**obj)
 
     class Config:
         allow_mutation = False
@@ -43,3 +55,13 @@ class GenericModel(BaseModel, PydanticGenericModel):
     Read: https://docs.pydantic.dev/usage/model_config/#change-behaviour-globally
     """
     pass
+
+
+def parse_hex_str(hex_str: Union[str, bytes]) -> bytes:
+    if isinstance(hex_str, str):
+        return bytes.fromhex(hex_str)
+
+    if not isinstance(hex_str, bytes):
+        raise TypeError(f'expected \'str\' or \'bytes\', got {hex_str}')
+
+    return hex_str
