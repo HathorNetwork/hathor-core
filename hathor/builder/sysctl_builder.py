1f5ecccead6144f4621e0fb3134b5d8d5c91530a
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-07-28 20:18:11 +0100

    feat: add --sysctl-init-file as cli command option

--- hathor/builder/sysctl_builder.py
+++ hathor/builder/sysctl_builder.py
@@ -18,6 +18,7 @@ from hathor.sysctl import ConnectionsManagerSysctl, Sysctl, WebsocketManagerSysc
 
 class SysctlBuilder:
     """Builder for the sysctl tree."""
+
     def __init__(self, artifacts: BuildArtifacts) -> None:
         self.artifacts = artifacts
 
