c3ba641cd9621c58f0dfc7ec44a981a3cba875e8
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-09-15 16:09:39 +0100

    feat: make init_file mandatory with assert

--- hathor/sysctl/init_file_loader.py
+++ hathor/sysctl/init_file_loader.py
@@ -1,19 +1,15 @@
-from typing import Optional
-
 from hathor.sysctl.runner import SysctlRunner
 
 
 class SysctlInitFileLoader:
-    def __init__(self, runner: SysctlRunner, init_file: Optional[str]) -> None:
+    def __init__(self, runner: SysctlRunner, init_file: str) -> None:
         assert runner
+        assert init_file
 
         self.runner = runner
         self.init_file = init_file
 
     def load(self) -> None:
-        if not self.init_file:
-            return
-
         with open(self.init_file, 'r', encoding='utf-8') as file:
             lines = file.readlines()
 
