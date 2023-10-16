fccf60c316c8c0495061f5a255c893670872f4d3
Luis Helder <luislhl@gmail.com>
2023-10-16 18:31:42 -0300

    feat: create a 'strict_status_code' query param

--- hathor/healthcheck/resources/healthcheck.py
+++ hathor/healthcheck/resources/healthcheck.py
@@ -1,5 +1,5 @@
 import hathor
-from hathor.api_util import Resource
+from hathor.api_util import Resource, get_arg_default, get_args
 from hathor.cli.openapi_files.register import register_resource
 from hathor.healthcheck.models import ComponentHealthCheck, ComponentType, HealthCheckStatus, ServiceHealthCheck
 from hathor.manager import HathorManager
@@ -29,8 +29,15 @@ class HealthcheckResource(Resource):
         """ GET request /health/
             Returns the health status of the fullnode
 
+            The 'strict_status_code' argument can be used to return 200 even if the fullnode is unhealthy.
+            This can be useful when integrating with tools that could prefer to pass the response code only
+            in case the response is 200.
+
             :rtype: string (json)
         """
+        raw_args = get_args(request)
+        strict_status_code = get_arg_default(raw_args, 'strict_status_code', '0') == '1'
+
         components_health_checks = [
             build_sync_health_status(self.manager)
         ]
@@ -40,7 +47,10 @@ class HealthcheckResource(Resource):
             checks={c.component_name: [c] for c in components_health_checks},
         )
 
-        request.setResponseCode(health_check.get_http_status_code())
+        if not strict_status_code:
+            status_code = health_check.get_http_status_code()
+            request.setResponseCode(status_code)
+
         return json_dumpb(health_check.to_json())
 
 
@@ -74,10 +84,27 @@ Returns 200 if the fullnode should be considered healthy.
 Returns 503 otherwise. The response will contain the components that were considered for the healthcheck
 and the reason why they were unhealthy.
 
+Returning 503 with a response body is not the standard behavior for our API, but it was chosen because
+most healthcheck tools expect a 503 response code to indicate that the service is unhealthy.
+
+Optionally, there is a query parameter 'strict_status_code' that can be used to return 200 even if the fullnode
+is unhealthy. When its value is 1, the response will always be 200.
+
 We currently perform 2 checks in the sync mechanism for the healthcheck:
 1. Whether the fullnode has recent block activity, i.e. if the fullnode has blocks with recent timestamps.
 2. Whether the fullnode has at least one synced peer
             ''',
+            'parameters': [
+                {
+                    'name': 'strict_status_code',
+                    'in': 'query',
+                    'description': 'Enables strict status code. If set to 1, the response will always be 200.',
+                    'required': False,
+                    'schema': {
+                        'type': 'string'
+                    }
+                },
+            ],
             'responses': {
                 '200': {
                     'description': 'Healthy',
