bbfb92f8c66d66262671bb46e2a8f74dbb769ae8
André Abadesso <andre.abadesso@gmail.com>
2024-07-29 10:13:25 -0300

    chore: force python3.10 in shellhook

--- flake.nix
+++ flake.nix
@@ -36,6 +36,7 @@
             shellHook = ''
               export CFLAGS="-I${pkgs.rocksdb}/include"
               export LDFLAGS="-L${pkgs.rocksdb}/lib"
+              poetry env use python3.10
             '';
           };
     });
