7b5ac2d768bba6eed7529951af9d32b4e39dbf3c
Gabriel Levcovitz <g.levcovitz@gmail.com>
2024-09-02 13:35:04 -0300

    implement bench_script_verification

--- hathor/cli/main.py
+++ hathor/cli/main.py
@@ -60,6 +60,7 @@ class CliManager:
             twin_tx,
             tx_generator,
             wallet,
+            bench_script_verification,
         )
         from .events_simulator import events_simulator
 
@@ -100,6 +101,8 @@ class CliManager:
         self.add_cmd('dev', 'x-import', db_import, 'EXPERIMENTAL: Import database from exported format.')
         self.add_cmd('dev', 'replay-logs', replay_logs, 'EXPERIMENTAL: re-play json logs as console printted')
         self.add_cmd('dev', 'load-from-logs', load_from_logs, 'Load vertices as they are found in a log dump')
+
+        self.add_cmd('bench', 'bench_script_verification', bench_script_verification, 'Benchmark script verification')
 
     def add_cmd(self, group: str, cmd: str, module: ModuleType, short_description: Optional[str] = None) -> None:
         self.command_list[cmd] = module
