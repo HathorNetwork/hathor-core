0703970f079d0355d3be3ab453b3beea0ac6c8ff
Gabriel Levcovitz <g.levcovitz@gmail.com>
2024-09-05 12:15:23 -0300

    feat: improve load-from-logs CLI command

--- hathor/cli/main.py
+++ hathor/cli/main.py
@@ -48,6 +48,7 @@ class CliManager:
             oracle_create_key,
             oracle_encode_data,
             oracle_get_pubkey,
+            parse_logs,
             peer_id,
             quick_test,
             replay_logs,
@@ -98,8 +99,10 @@ class CliManager:
         self.add_cmd('dev', 'events_simulator', events_simulator, 'Simulate integration events via websocket')
         self.add_cmd('dev', 'x-export', db_export, 'EXPERIMENTAL: Export database to a simple format.')
         self.add_cmd('dev', 'x-import', db_import, 'EXPERIMENTAL: Import database from exported format.')
-        self.add_cmd('dev', 'replay-logs', replay_logs, 'EXPERIMENTAL: re-play json logs as console printted')
-        self.add_cmd('dev', 'load-from-logs', load_from_logs, 'Load vertices as they are found in a log dump')
+        self.add_cmd('dev', 'replay-logs', replay_logs, 'EXPERIMENTAL: re-play json logs as console printed')
+        self.add_cmd('dev', 'load-from-logs', load_from_logs,
+                     'Load vertices as they are found in a log dump that was parsed with parse-logs')
+        self.add_cmd('dev', 'parse-logs', parse_logs, 'Parse a log dump to use it with load-from-logs')
 
     def add_cmd(self, group: str, cmd: str, module: ModuleType, short_description: Optional[str] = None) -> None:
         self.command_list[cmd] = module
