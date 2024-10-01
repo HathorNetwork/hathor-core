24a6e8ac5bbe1f51ac5912b9b537951864fa626f
Gabriel Levcovitz <g.levcovitz@gmail.com>
2024-10-01 17:58:11 -0300

    wip

--- hathor/p2p/netfilter/context.py
+++ hathor/p2p/netfilter/context.py
@@ -17,15 +17,13 @@ from typing import TYPE_CHECKING, Optional
 if TYPE_CHECKING:
     from twisted.internet.interfaces import IAddress
 
-    from hathor.p2p.manager import ConnectionsManager
     from hathor.p2p.protocol import HathorProtocol
 
 
 class NetfilterContext:
     """Context sent to the targets when a match occurs."""
-    def __init__(self, *, connections: Optional['ConnectionsManager'] = None, addr: Optional['IAddress'] = None,
+    def __init__(self, *, addr: Optional['IAddress'] = None,
                  protocol: Optional['HathorProtocol'] = None):
         """Initialize the context."""
         self.addr = addr
         self.protocol = protocol
-        self.connections = connections
