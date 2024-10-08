35d95c20f97525192ad308d720c9d2be8ebfe25c
Gabriel Levcovitz <g.levcovitz@gmail.com>
2024-10-08 17:20:32 -0300

    remove migration

--- hathor/transaction/storage/migrations/add_closest_ancestor_block.py
+++ hathor/transaction/storage/migrations/add_closest_ancestor_block.py
@@ -16,11 +16,7 @@ from typing import TYPE_CHECKING
 
 from structlog import get_logger
 
-from hathor.conf.get_settings import get_global_settings
-from hathor.transaction import Transaction
-from hathor.transaction.static_metadata import TransactionStaticMetadata
 from hathor.transaction.storage.migrations import BaseMigration
-from hathor.util import progress
 
 if TYPE_CHECKING:
     from hathor.transaction.storage import TransactionStorage
@@ -36,20 +32,6 @@ class Migration(BaseMigration):
         return 'add_closest_ancestor_block'
 
     def run(self, storage: 'TransactionStorage') -> None:
-        """This migration populates the closest_ancestor_block static metadata attribute of Transactions."""
-        log = logger.new()
-        settings = get_global_settings()
-        topological_iter = storage.topological_iterator()
-
-        for vertex in progress(topological_iter, log=log, total=None):
-            if not isinstance(vertex, Transaction):
-                # blocks don't have this attribute
-                continue
-
-            # We calculate the whole static metadata, which should be the same except for the closest_ancestor_block
-            new_static_metadata = TransactionStaticMetadata.create_from_storage(vertex, settings, storage)
-            assert vertex.static_metadata == new_static_metadata.copy(update=dict(closest_ancestor_block=b''))
-
-            # We set the new static metadata manually and save it
-            vertex._static_metadata = new_static_metadata
-            storage.save_transaction(vertex, only_metadata=True)
+        raise Exception('Cannot migrate your database due to an incompatible change in the metadata. '
+                        'Please, delete your data folder and use the latest available snapshot or sync '
+                        'from beginning.')
