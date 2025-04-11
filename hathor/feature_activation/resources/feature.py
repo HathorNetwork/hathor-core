fd94060ee08f873fafed3b9d85b5f500aa2565b4
raul-oliveira <mrraul65@gmail.com>
2025-04-11 11:38:19 -0300

    revert feature activation changes

--- hathor/feature_activation/resources/feature.py
+++ hathor/feature_activation/resources/feature.py
@@ -71,14 +71,14 @@ class FeatureResource(Resource):
         feature_infos = self._feature_service.get_feature_infos(vertex=block)
 
         for feature, feature_info in feature_infos.items():
-            if not feature_info.criteria.is_block_in_activation_range(block.static_metadata.height):
+            if feature_info.state not in FeatureState.get_signaling_states():
                 continue
 
             block_feature = GetBlockFeatureResponse(
                 bit=feature_info.criteria.bit,
                 signal=block.get_feature_activation_bit_value(feature_info.criteria.bit),
                 feature=feature,
-                feature_state=feature_info.state.name,
+                feature_state=feature_info.state.name
             )
 
             signal_bits.append(block_feature)
