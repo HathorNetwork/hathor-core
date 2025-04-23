546267ea1effb37910b6001698ecb7808e3f6459
Jan Segre <jan@hathor.network>
2025-04-23 16:17:26 +0200

    feat(nano): add block data to the context through vertex data

--- hathor/nanocontracts/vertex_data.py
+++ hathor/nanocontracts/vertex_data.py
@@ -17,10 +17,30 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+from typing_extensions import Self
+
 from hathor.types import TokenUid, VertexId
 
 if TYPE_CHECKING:
-    from hathor.transaction import BaseTransaction, TxInput, TxOutput, TxVersion
+    from hathor.transaction import BaseTransaction, Block, TxInput, TxOutput, TxVersion
+
+
+def _get_txin_output(vertex: BaseTransaction, txin: TxInput) -> TxOutput | None:
+    """Return the output that txin points to."""
+    from hathor.transaction.storage.exceptions import TransactionDoesNotExist
+
+    if vertex.storage is None:
+        return None
+
+    try:
+        vertex2 = vertex.storage.get_transaction(txin.tx_id)
+    except TransactionDoesNotExist:
+        assert False, f'missing dependency: {txin.tx_id.hex()}'
+
+    assert len(vertex2.outputs) > txin.index, 'invalid output index'
+
+    txin_output = vertex2.outputs[txin.index]
+    return txin_output
 
 
 @dataclass(frozen=True, slots=True, kw_only=True)
@@ -34,41 +54,33 @@ class VertexData:
     outputs: tuple[TxOutputData, ...]
     tokens: tuple[TokenUid, ...]
     parents: tuple[VertexId, ...]
-
-    @staticmethod
-    def _get_txin_output(vertex: BaseTransaction, txin: TxInput) -> TxOutput | None:
-        """Return the output that txin points to."""
-        from hathor.transaction.storage.exceptions import TransactionDoesNotExist
-
-        if vertex.storage is None:
-            return None
-
-        try:
-            vertex2 = vertex.storage.get_transaction(txin.tx_id)
-        except TransactionDoesNotExist:
-            assert False, f'missing dependency: {txin.tx_id.hex()}'
-
-        assert len(vertex2.outputs) > txin.index, 'invalid output index'
-
-        txin_output = vertex2.outputs[txin.index]
-        return txin_output
+    block: BlockData
 
     @classmethod
-    def create_from_vertex(cls, vertex: BaseTransaction) -> VertexData:
+    def create_from_vertex(cls, vertex: BaseTransaction) -> Self:
         inputs = tuple(
-            TxInputData.create_from_txin(txin, cls._get_txin_output(vertex, txin))
+            TxInputData.create_from_txin(txin, _get_txin_output(vertex, txin))
             for txin in vertex.inputs
         )
         outputs = tuple(TxOutputData.create_from_txout(txout) for txout in vertex.outputs)
         parents = tuple(vertex.parents)
         tokens: tuple[TokenUid, ...] = tuple()
+        vertex_meta = vertex.get_metadata()
+        if vertex_meta.first_block is not None:
+            assert vertex.storage is not None
+            assert vertex_meta.first_block is not None
+            block = vertex.storage.get_block(vertex_meta.first_block)
+            block_data = BlockData.create_from_block(block)
+        else:
+            # XXX: use dummy data instead
+            block_data = BlockData(hash=VertexId(b''), timestamp=0, height=0)
 
         original_tokens = getattr(vertex, 'tokens', None)
         if original_tokens is not None:
             # XXX Should we add HTR_TOKEN_ID as first token?
             tokens = tuple(original_tokens)
 
-        return VertexData(
+        return cls(
             version=vertex.version,
             hash=vertex.hash,
             nonce=vertex.nonce,
@@ -78,6 +90,7 @@ class VertexData:
             outputs=outputs,
             tokens=tokens,
             parents=parents,
+            block=block_data,
         )
 
 
@@ -89,8 +102,8 @@ class TxInputData:
     info: TxOutputData | None
 
     @classmethod
-    def create_from_txin(cls, txin: TxInput, txin_output: TxOutput | None) -> TxInputData:
-        return TxInputData(
+    def create_from_txin(cls, txin: TxInput, txin_output: TxOutput | None) -> Self:
+        return cls(
             tx_id=txin.tx_id,
             index=txin.index,
             data=txin.data,
@@ -105,9 +118,24 @@ class TxOutputData:
     token_data: int
 
     @classmethod
-    def create_from_txout(cls, txout: TxOutput) -> TxOutputData:
-        return TxOutputData(
+    def create_from_txout(cls, txout: TxOutput) -> Self:
+        return cls(
             value=txout.value,
             script=txout.script,
             token_data=txout.token_data,
+        )
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class BlockData:
+    hash: VertexId
+    timestamp: int
+    height: int
+
+    @classmethod
+    def create_from_block(cls, block: Block) -> Self:
+        return cls(
+            hash=block.hash,
+            timestamp=block.timestamp,
+            height=block.get_height(),
         )
