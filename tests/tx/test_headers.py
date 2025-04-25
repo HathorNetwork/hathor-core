284ace2aefe017b2cde288bbcc8e57e2f3fa093c
Gabriel Levcovitz <g.levcovitz@gmail.com>
2025-04-25 13:05:51 -0300

    fix(headers): missing headers on token creation tx sighash (#195)

--- tests/tx/test_headers.py
+++ tests/tx/test_headers.py
@@ -1,14 +1,15 @@
 import pytest
 
 from hathor.exception import InvalidNewTransaction
-from hathor.nanocontracts import Blueprint, Context, public
+from hathor.nanocontracts import Blueprint, Context, OnChainBlueprint, public
 from hathor.nanocontracts.nanocontract import DeprecatedNanoContract
 from hathor.nanocontracts.types import NCActionType
 from hathor.transaction import BaseTransaction, Block, Transaction
 from hathor.transaction.exceptions import HeaderNotSupported
-from hathor.transaction.headers import NanoHeader
+from hathor.transaction.headers import NanoHeader, VertexBaseHeader
 from hathor.transaction.headers.nano_header import NanoHeaderAction
 from hathor.transaction.token_creation_tx import TokenCreationTransaction
+from hathor.transaction.util import VerboseCallback
 from tests import unittest
 
 
@@ -22,6 +23,24 @@ class MyTestBlueprint(Blueprint):
         pass
 
 
+class FakeHeader(VertexBaseHeader):
+    @classmethod
+    def deserialize(
+        cls,
+        tx: BaseTransaction,
+        buf: bytes,
+        *,
+        verbose: VerboseCallback = None,
+    ) -> tuple[VertexBaseHeader, bytes]:
+        raise NotImplementedError
+
+    def serialize(self) -> bytes:
+        return b'fake header'
+
+    def get_sighash_bytes(self) -> bytes:
+        return b'fake sighash'
+
+
 class VertexHeadersTest(unittest.TestCase):
     def has_nano_header(self, vertex: BaseTransaction) -> bool:
         for header in vertex.headers:
@@ -29,22 +48,25 @@ class VertexHeadersTest(unittest.TestCase):
                 return True
         return False
 
-    def test_allowed_vertices(self) -> None:
-        blueprint_id = b'x' * 32
+    def setUp(self) -> None:
+        super().setUp()
+        self.blueprint_id = b'x' * 32
+        self.manager = self.create_peer('testnet')
+        self.manager.tx_storage.nc_catalog.blueprints[self.blueprint_id] = MyTestBlueprint
+        self.dag_builder = self.get_dag_builder(self.manager)
 
-        manager = self.create_peer('testnet')
-        manager.tx_storage.nc_catalog.blueprints[blueprint_id] = MyTestBlueprint
+        private_key = unittest.OCB_TEST_PRIVKEY.hex()
+        password = unittest.OCB_TEST_PASSWORD.hex()
 
-        dag_builder = self.get_dag_builder(manager)
-        artifacts = dag_builder.build_from_str(f'''
+        self.artifacts = self.dag_builder.build_from_str(f'''
             blockchain genesis b[1..12]
             b10 < dummy
 
-            nc1.nc_id = "{blueprint_id.hex()}"
+            nc1.nc_id = "{self.blueprint_id.hex()}"
             nc1.nc_method = initialize()
 
             nc2.type = NanoContract
-            nc2.nc_id = "{blueprint_id.hex()}"
+            nc2.nc_id = "{self.blueprint_id.hex()}"
             nc2.nc_method = initialize()
 
             tx1.out[0] = 5 TKA
@@ -59,39 +81,139 @@ class VertexHeadersTest(unittest.TestCase):
             TKB.nc_id = nc1
             TKB.nc_method = nop()
 
-            dummy < b11 < nc1 < nc2 < TKA < tx1 < b12 < TKB < tx2
+            ocb1.ocb_private_key = "{private_key}"
+            ocb1.ocb_password = "{password}"
+            ocb1.ocb_code = test_blueprint1.py, TestBlueprint1
+
+            dummy < b11 < nc1 < nc2 < TKA < tx1 < b12 < TKB < tx2 < ocb1
         ''')
+        self.artifacts.propagate_with(self.manager, up_to='dummy')
 
-        artifacts.propagate_with(manager, up_to='dummy')
-
-        vertex: BaseTransaction
-
-        expected_to_pass: list[tuple[str, type[BaseTransaction], bool]] = [
+        self.valid_vertices: list[tuple[str, type[BaseTransaction], bool]] = [
             ('b11', Block, False),
             ('nc1', Transaction, True),
             ('nc2', DeprecatedNanoContract, True),
             ('TKA', TokenCreationTransaction, False),
             ('TKB', TokenCreationTransaction, True),
             ('tx1', Transaction, False),
+            ('ocb1', OnChainBlueprint, False),
+            # TODO: We should also test MergeMinedBlock, but the DAGBuilder doesn't support it yet
         ]
-        for name, _type, should_have_nano_header in expected_to_pass:
-            vertex = artifacts.get_typed_vertex(name, _type)
+
+    def test_headers_affect_hash(self) -> None:
+        for name, type_, is_nano in self.valid_vertices:
+            vertex: BaseTransaction = self.artifacts.get_typed_vertex(name, type_)
+            assert self.has_nano_header(vertex) == is_nano
+
+            if type_ is DeprecatedNanoContract:
+                # DeprecatedNanoContract will be removed before it supports
+                # any other headers, so we just skip testing it for now.
+                continue
+
+            # Test adding a new header.
+            msg = f'changing headers should change the hash on "{name}"'
+            clone = vertex.clone(include_storage=False, include_metadata=False)
+            assert clone.hash == clone.calculate_hash()
+            clone.headers.append(FakeHeader())
+            assert clone.hash != clone.calculate_hash(), msg
+
+            # Now we'll test nano header attributes, so we can skip non-nano txs
+            if not is_nano:
+                continue
+
+            assert isinstance(vertex, Transaction)
+            attributes_and_new_values = [
+                ('nc_version', 123),
+                ('nc_id', b'123'),
+                ('nc_method', 'new_method'),
+                ('nc_args_bytes', b'new args'),
+                ('nc_actions', [NanoHeaderAction(type=NCActionType.DEPOSIT, token_index=0, amount=123)]),
+                ('nc_pubkey', b'new key'),
+                ('nc_signature', b'new signature'),
+            ]
+
+            # Test editing existing nano header.
+            for attribute, new_value in attributes_and_new_values:
+                clone = vertex.clone(include_storage=False, include_metadata=False)
+                assert clone.hash == vertex.hash
+                assert clone.hash == clone.calculate_hash()
+                setattr(clone.get_nano_header(), attribute, new_value)
+                assert clone.hash != clone.calculate_hash(), msg
+
+    def test_headers_affect_sighash_all(self) -> None:
+        for name, type_, is_nano in self.valid_vertices:
+            vertex: BaseTransaction = self.artifacts.get_typed_vertex(name, type_)
+            assert self.has_nano_header(vertex) == is_nano
+
+            if type_ is DeprecatedNanoContract:
+                # DeprecatedNanoContract will be removed before it supports
+                # any other headers, so we just skip testing it for now.
+                continue
+
+            if not isinstance(vertex, Transaction):
+                # only transactions have sighash
+                continue
+
+            # Test adding a new header.
+            msg = f'changing headers should change the sighash on "{name}"'
+            clone = vertex.clone(include_storage=False, include_metadata=False)
+            sighash_before = clone.get_sighash_all(skip_cache=True)
+            assert sighash_before == vertex.get_sighash_all(skip_cache=True)
+            clone.headers.append(FakeHeader())
+            sighash_after = clone.get_sighash_all(skip_cache=True)
+            assert sighash_before != sighash_after, msg
+
+            # Now we'll test nano header attributes, so we can skip non-nano txs
+            if not is_nano:
+                continue
+
+            assert isinstance(vertex, Transaction)
+            attributes_and_new_values = [
+                ('nc_version', 123),
+                ('nc_id', b'123'),
+                ('nc_method', 'new_method'),
+                ('nc_args_bytes', b'new args'),
+                ('nc_actions', [NanoHeaderAction(type=NCActionType.DEPOSIT, token_index=0, amount=123)]),
+                ('nc_pubkey', b'new key'),
+            ]
+
+            # Test editing existing nano header.
+            for attribute, new_value in attributes_and_new_values:
+                clone = vertex.clone(include_storage=False, include_metadata=False)
+                sighash_before = clone.get_sighash_all(skip_cache=True)
+                assert sighash_before == vertex.get_sighash_all(skip_cache=True)
+                setattr(clone.get_nano_header(), attribute, new_value)
+                sighash_after = clone.get_sighash_all(skip_cache=True)
+                assert sighash_before != sighash_after, msg
+
+            # Changing the nc_signature does not affect sighash all.
+            clone = vertex.clone(include_storage=False, include_metadata=False)
+            sighash_before = clone.get_sighash_all(skip_cache=True)
+            assert sighash_before == vertex.get_sighash_all(skip_cache=True)
+            clone.get_nano_header().nc_signature = b'new signature'
+            sighash_after = clone.get_sighash_all(skip_cache=True)
+            assert sighash_before == sighash_after, msg
+
+    def test_nano_header_allowed_vertices(self) -> None:
+        for name, _type, should_have_nano_header in self.valid_vertices:
+            vertex: BaseTransaction = self.artifacts.get_typed_vertex(name, _type)
             assert self.has_nano_header(vertex) == should_have_nano_header
-            vertex.storage = manager.tx_storage
+            vertex.storage = self.manager.tx_storage
             clone = vertex.clone(include_metadata=False, include_storage=True)
             assert bytes(clone) == bytes(vertex)
-            assert manager.on_new_tx(vertex, fails_silently=False)
+            assert self.manager.on_new_tx(vertex, fails_silently=False)
 
         expected_to_fail: list[tuple[str, type[BaseTransaction], bool]] = [
             ('b12', Block, True),
         ]
+
         for name, _type, should_have_nano_header in expected_to_fail:
-            vertex = artifacts.get_typed_vertex(name, _type)
+            vertex = self.artifacts.get_typed_vertex(name, _type)
             assert self.has_nano_header(vertex) == should_have_nano_header
             clone = _type.create_from_struct(bytes(vertex))
             assert bytes(clone) == bytes(vertex)
             with pytest.raises(InvalidNewTransaction) as e:
-                manager.on_new_tx(vertex, fails_silently=False)
+                self.manager.on_new_tx(vertex, fails_silently=False)
             assert isinstance(e.value.__cause__, HeaderNotSupported)
 
     def test_nano_header_round_trip(self) -> None:
