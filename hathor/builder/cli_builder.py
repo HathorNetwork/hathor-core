84a4de9886dca02d1298f76c76597bfd78089d45
Gabriel Levcovitz <g.levcovitz@gmail.com>
2025-06-03 19:33:12 -0300

    feat(nano): implement consensus

--- hathor/builder/cli_builder.py
+++ hathor/builder/cli_builder.py
@@ -33,6 +33,7 @@ from hathor.feature_activation.storage.feature_activation_storage import Feature
 from hathor.indexes import IndexesManager, RocksDBIndexesManager
 from hathor.manager import HathorManager
 from hathor.mining.cpu_mining_service import CpuMiningService
+from hathor.nanocontracts.nc_exec_logs import NCLogStorage
 from hathor.nanocontracts.runner.runner import RunnerFactory
 from hathor.p2p.manager import ConnectionsManager
 from hathor.p2p.peer import PrivatePeer
@@ -239,6 +240,9 @@ class CliBuilder:
             self.log.debug('enable nano indices')
             tx_storage.indexes.enable_nc_indices()
 
+        from hathor.nanocontracts.sorter.timestamp_sorter import timestamp_nc_calls_sorter
+        nc_calls_sorter = timestamp_nc_calls_sorter
+
         assert self.nc_storage_factory is not None
         runner_factory = RunnerFactory(
             reactor=reactor,
@@ -247,10 +251,21 @@ class CliBuilder:
             nc_storage_factory=self.nc_storage_factory,
         )
 
+        nc_log_storage = NCLogStorage(
+            settings=settings,
+            path=self.rocksdb_storage.path,
+            config=self._args.nc_exec_logs,
+        )
+
         soft_voided_tx_ids = set(settings.SOFT_VOIDED_TX_IDS)
         consensus_algorithm = ConsensusAlgorithm(
+            self.nc_storage_factory,
             soft_voided_tx_ids,
             pubsub=pubsub,
+            settings=settings,
+            runner_factory=runner_factory,
+            nc_log_storage=nc_log_storage,
+            nc_calls_sorter=nc_calls_sorter,
         )
 
         if self._args.x_enable_event_queue or self._args.enable_event_queue:
