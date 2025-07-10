c7643e01790639c54fe1451d03d5a6a05e77ac04
Gabriel Levcovitz <g.levcovitz@gmail.com>
2025-07-10 17:14:14 -0300

    feat(nano): add check-blueprint CLI

--- hathor/verification/verification_service.py
+++ hathor/verification/verification_service.py
@@ -330,7 +330,4 @@ class VerificationService:
         self._verify_without_storage_tx(tx)
         self.verifiers.on_chain_blueprint.verify_pubkey_is_allowed(tx)
         self.verifiers.on_chain_blueprint.verify_nc_signature(tx)
-        self.verifiers.on_chain_blueprint.verify_python_script(tx)
-        self.verifiers.on_chain_blueprint.verify_script_restrictions(tx)
-        self.verifiers.on_chain_blueprint.verify_has_blueprint_attr(tx)
-        self.verifiers.on_chain_blueprint.verify_blueprint_type(tx)
+        self.verifiers.on_chain_blueprint.verify_code(tx)
