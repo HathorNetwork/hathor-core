7490a17e8c7336786ab4e940dabe2b732f3480d3
LFRezende <lfsrsprofessional@gmail.com>
2025-07-09 21:54:10 -0300

    Fix: test_capabilities.py
    CAPABILITIES_WHITELIST is now definitive to block a handle_hello, meaning it must be set up for connection to pass

--- tests/p2p/test_capabilities.py
+++ tests/p2p/test_capabilities.py
@@ -1,4 +1,4 @@
-from hathor.p2p.states import ReadyState
+from hathor.p2p.states import ReadyState, PeerIdState, HelloState
 from hathor.p2p.sync_v2.agent import NodeBlockSync
 from hathor.simulator import FakeConnection
 from tests import unittest
@@ -9,8 +9,13 @@ class CapabilitiesTestCase(unittest.TestCase):
         network = 'testnet'
         manager1 = self.create_peer(network, capabilities=[self._settings.CAPABILITY_WHITELIST,
                                                            self._settings.CAPABILITY_SYNC_VERSION])
-        manager2 = self.create_peer(network, capabilities=[self._settings.CAPABILITY_SYNC_VERSION])
+        manager2 = self.create_peer(network, capabilities=[self._settings.CAPABILITY_WHITELIST, self._settings.CAPABILITY_SYNC_VERSION])
 
+        assert manager1.connections.peers_whitelist is not None, 'Peers whitelist should not be None'
+        assert manager2.connections.peers_whitelist is not None, 'Peers whitelist should not be None'
+        assert len(manager1.connections.peers_whitelist._current) == 0, 'Should have no peers in the whitelist'
+        assert len(manager2.connections.peers_whitelist._current) == 0, 'Should have no peers in the whitelist'
+        
         conn = FakeConnection(manager1, manager2)
 
         # Run the p2p protocol.
@@ -18,29 +23,12 @@ class CapabilitiesTestCase(unittest.TestCase):
             conn.run_one_step(debug=True)
             self.clock.advance(0.1)
 
-        # Even if we don't have the capability we must connect because the whitelist url conf is None
+        # Update: Now, the URL has no effect to block the handle hello in HelloState -
+        # having capability or not is definitive to  block the conn.
+        # Also, no more need to create two connections, one is enough to test the capabilities.
         assert isinstance(conn._proto1.state, ReadyState)
         assert isinstance(conn._proto2.state, ReadyState)
         self.assertEqual(conn._proto1.state.state_name, 'READY')
         self.assertEqual(conn._proto2.state.state_name, 'READY')
         self.assertIsInstance(conn._proto1.state.sync_agent, NodeBlockSync)
         self.assertIsInstance(conn._proto2.state.sync_agent, NodeBlockSync)
-
-        manager3 = self.create_peer(network, capabilities=[self._settings.CAPABILITY_WHITELIST,
-                                                           self._settings.CAPABILITY_SYNC_VERSION])
-        manager4 = self.create_peer(network, capabilities=[self._settings.CAPABILITY_WHITELIST,
-                                                           self._settings.CAPABILITY_SYNC_VERSION])
-
-        conn2 = FakeConnection(manager3, manager4)
-
-        # Run the p2p protocol.
-        for _ in range(100):
-            conn2.run_one_step(debug=True)
-            self.clock.advance(0.1)
-
-        assert isinstance(conn2._proto1.state, ReadyState)
-        assert isinstance(conn2._proto2.state, ReadyState)
-        self.assertEqual(conn2._proto1.state.state_name, 'READY')
-        self.assertEqual(conn2._proto2.state.state_name, 'READY')
-        self.assertIsInstance(conn2._proto1.state.sync_agent, NodeBlockSync)
-        self.assertIsInstance(conn2._proto2.state.sync_agent, NodeBlockSync)
