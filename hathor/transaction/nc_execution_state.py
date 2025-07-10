e417e0efa519d7beb24aa10ab5eb847196791bb3
Gabriel Levcovitz <g.levcovitz@gmail.com>
2025-07-10 19:57:03 -0300

    fix(nano): non-idempotent indexes handling (#1313)

--- hathor/transaction/nc_execution_state.py
+++ hathor/transaction/nc_execution_state.py
@@ -18,6 +18,6 @@ from enum import StrEnum, auto, unique
 @unique
 class NCExecutionState(StrEnum):
     PENDING = auto()  # aka, not even tried to execute it
-    SUCCESS = auto()  # execution was sucessful
+    SUCCESS = auto()  # execution was successful
     FAILURE = auto()  # execution failed and the transaction is voided
     SKIPPED = auto()  # execution was skipped, usually because the transaction was voided
