8652a5959a1a9b66b403ae66fbd32cc37e78faed
Luis Helder <luislhl@gmail.com>
2025-07-22 11:58:33 -0300

    style: fix linters

--- hathor/nanocontracts/resources/__init__.py
+++ hathor/nanocontracts/resources/__init__.py
@@ -17,9 +17,9 @@ from hathor.nanocontracts.resources.blueprint_source_code import BlueprintSource
 from hathor.nanocontracts.resources.builtin import BlueprintBuiltinResource
 from hathor.nanocontracts.resources.history import NanoContractHistoryResource
 from hathor.nanocontracts.resources.nc_creation import NCCreationResource
+from hathor.nanocontracts.resources.nc_exec_logs import NCExecLogsResource
 from hathor.nanocontracts.resources.on_chain import BlueprintOnChainResource
 from hathor.nanocontracts.resources.state import NanoContractStateResource
-from hathor.nanocontracts.resources.nc_exec_logs import NCExecLogsResource
 
 __all__ = [
     'BlueprintBuiltinResource',
