bcac907708d3c12c9490172ef89c4354a81b66fe
Gabriel Levcovitz <g.levcovitz@gmail.com>
2025-08-01 20:02:39 -0300

    WIPAO - FrozenObj wrapper working with Context

--- hathor/nanocontracts/faux_immutability.py
+++ hathor/nanocontracts/faux_immutability.py
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from typing import final
+
 
 class _FauxImmutabilityMeta(type):
     __slots__ = ()
@@ -29,7 +31,7 @@ class _FauxImmutabilityMeta(type):
         # TODO: Check all possible magic methods/attrs to add here
         prohibited_attrs = (
             '__new__',
-            '__call__',
+            # '__call__',
             '__setattr__',
             '__delattr__',
         )
@@ -76,3 +78,45 @@ def __set_faux_immutable__(obj: FauxImmutable, name: str, value: object) -> None
     if name.startswith('__') and not name.endswith('__'):
         name = f'_{obj.__class__.__name__}{name}'
     object.__setattr__(obj, name, value)
+
+def __freeze_obj__(obj: object) -> object:
+    allowed_attrs = getattr(obj, '__ALLOWED_ATTRS__', ())
+    allowed_methods = getattr(obj, '__ALLOWED_METHODS__', ())
+
+    if not allowed_attrs or not allowed_methods:
+        # TODO: Can't freeze
+        return obj
+
+    return FrozenObj(obj=obj, allowed_attrs=allowed_attrs, allowed_methods=allowed_methods)
+
+
+@final
+class FrozenCallable(FauxImmutable):
+    __slots__ = ('__callable',)
+
+    def __init__(self, callable: object) -> None:
+        __set_faux_immutable__(self, '__callable', callable)
+
+    def __call__(self, *args, **kwargs):
+        return self.__callable(*args, **kwargs)
+
+@final
+class FrozenObj(FauxImmutable):
+    __slots__ = ('__obj', '__allowed_attrs', '__allowed_methods')
+
+    def __init__(self, *, obj: object, allowed_attrs: set[str], allowed_methods: set[str]) -> None:
+        __set_faux_immutable__(self, '__obj', obj)
+        __set_faux_immutable__(self, '__allowed_attrs', allowed_attrs)
+        __set_faux_immutable__(self, '__allowed_methods', allowed_methods)
+
+    def __getattr__(self, name):
+        if name in self.__allowed_attrs:
+            return getattr(self.__obj, name)
+
+        if name in self.__allowed_methods:
+            return FrozenCallable(getattr(self.__obj, name))
+
+        raise AttributeError(f'FORBIDDEN! FORBIDDEN! {name}')
+
+    def is_(self, obj: object) -> bool:
+        return self.__obj is obj
