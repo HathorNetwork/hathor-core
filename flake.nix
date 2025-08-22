6b58bfa4534d6c76f3da87f24989dacc457f46ef
André Abadesso <andre.abadesso@gmail.com>
2025-08-22 16:37:39 -0300

    feat: voided transaction chain scenario

--- flake.nix
+++ flake.nix
@@ -1,40 +1,58 @@
 {
   description = "virtual environments";
 
-  inputs.devshell.url = "github:numtide/devshell";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, flake-utils, devshell, nixpkgs }:
-
+  outputs = { self, flake-utils, devshell, nixpkgs, ... }:
+    let
+      overlays.default = final: prev: {
+        nodejs = final.nodejs_24;
+        nodePackages = prev.nodePackages;
+      };
+    in
     flake-utils.lib.eachDefaultSystem (system: {
-      devShells.default =
+      devShell =
         let
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [ devshell.overlays.default ];
+            overlays = [
+              devshell.overlays.default
+              overlays.default
+            ];
+            # Always allow unfree here
+            config = {
+              allowUnfree = true;
+            };
           };
         in
-          pkgs.mkShell {
-            buildInputs = [
-              pkgs.python312
-              pkgs.poetry
-              pkgs.rocksdb
-              pkgs.snappy
-              pkgs.openssl
-              pkgs.readline
-              pkgs.zlib
-              pkgs.xz
-              pkgs.bzip2
-              pkgs.lz4
-              pkgs.cmake
-            ];
+        pkgs.devshell.mkShell {
+          packages = with pkgs; [
+            nixpkgs-fmt
+            nodejs_24
+            yarn-berry
+            claude-code
+            python312
+            poetry
+            rocksdb
+            snappy
+            openssl
+            readline
+            zlib
+            xz
+            bzip2
+            lz4
+            cmake
+          ];
 
-            shellHook = ''
-              export CFLAGS="-I${pkgs.rocksdb}/include"
-              export LDFLAGS="-L${pkgs.rocksdb}/lib"
-              poetry env use python3.12
-            '';
-          };
+          devshell.startup.shell-hook.text = ''
+            export CFLAGS="-I${pkgs.rocksdb}/include"
+            export LDFLAGS="-L${pkgs.rocksdb}/lib"
+            poetry env use python3.12
+          '';
+        };
     });
 }
