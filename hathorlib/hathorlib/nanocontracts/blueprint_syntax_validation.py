08e08ba997f9c1da95782ac2994bebfc576d2509
André Carneiro <andreluizmrcarneiro@gmail.com>
2026-02-26 12:08:09 -0300

    feat: Move core types, decorators, and address utilities to hathorlib

---
+++ hathorlib/hathorlib/nanocontracts/blueprint_syntax_validation.py
@@ -0,0 +1,77 @@
+#  Copyright 2025 Hathor Labs
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from __future__ import annotations
+
+import inspect
+from typing import Callable
+
+from hathorlib.nanocontracts.exception import BlueprintSyntaxError
+
+
+def validate_has_self_arg(fn: Callable, annotation_name: str) -> None:
+    """Validate the `self` arg of a callable."""
+    arg_spec = inspect.getfullargspec(fn)
+    if len(arg_spec.args) == 0:
+        raise BlueprintSyntaxError(f'@{annotation_name} method must have `self` argument: `{fn.__name__}()`')
+
+    if arg_spec.args[0] != 'self':
+        raise BlueprintSyntaxError(
+            f'@{annotation_name} method first argument must be called `self`: `{fn.__name__}()`'
+        )
+
+    if 'self' in arg_spec.annotations.keys():
+        raise BlueprintSyntaxError(f'@{annotation_name} method `self` argument must not be typed: `{fn.__name__}()`')
+
+
+def validate_method_types(fn: Callable) -> None:
+    """Validate the arg and return types of a callable."""
+    special_args = ['self']
+    arg_spec = inspect.getfullargspec(fn)
+
+    if 'return' not in arg_spec.annotations:
+        raise BlueprintSyntaxError(f'missing return type on method `{fn.__name__}`')
+
+    for arg_name in arg_spec.args:
+        if arg_name in special_args:
+            continue
+
+        if arg_name not in arg_spec.annotations:
+            raise BlueprintSyntaxError(f'argument `{arg_name}` on method `{fn.__name__}` must be typed')
+
+
+def validate_has_ctx_arg(fn: Callable, annotation_name: str) -> None:
+    """Validate the context arg of a callable."""
+    arg_spec = inspect.getfullargspec(fn)
+
+    if len(arg_spec.args) < 2:
+        raise BlueprintSyntaxError(
+            f'@{annotation_name} method must have `Context` argument: `{fn.__name__}()`'
+        )
+
+    from hathorlib.nanocontracts.context import Context
+    second_arg = arg_spec.args[1]
+    if arg_spec.annotations[second_arg] is not Context:
+        raise BlueprintSyntaxError(
+            f'@{annotation_name} method second arg `{second_arg}` argument must be of type `Context`: '
+            f'`{fn.__name__}()`'
+        )
+
+
+def validate_has_not_ctx_arg(fn: Callable, annotation_name: str) -> None:
+    """Validate that a callable doesn't have a `Context` arg."""
+    from hathorlib.nanocontracts.context import Context
+    arg_spec = inspect.getfullargspec(fn)
+    if Context in arg_spec.annotations.values():
+        raise BlueprintSyntaxError(f'@{annotation_name} method cannot have arg with type `Context`: `{fn.__name__}()`')
--- hathorlib/hathorlib/utils/__init__.py
+++ hathorlib/hathorlib/utils/__init__.py
@@ -4,22 +4,29 @@ Copyright (c) Hathor Labs and its affiliates.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
-import hashlib
 import re
 import struct
-from typing import Any, Tuple, Union, cast
+from typing import Any, Tuple, Union
 
-import base58
-from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
-
-from hathorlib.conf import HathorSettings
-from hathorlib.exceptions import InvalidAddress
 from hathorlib.serialization import Deserializer, SerializationError, Serializer
 from hathorlib.serialization.adapters import MaxBytesExceededError
 from hathorlib.serialization.encoding.leb128 import decode_leb128, encode_leb128
 
-settings = HathorSettings()
+# Re-export address utilities from the dedicated module for backward compatibility
+from hathorlib.utils.address import (  # noqa: F401
+    decode_address,
+    get_address_b58_from_bytes,
+    get_address_b58_from_public_key,
+    get_address_b58_from_public_key_bytes,
+    get_address_b58_from_public_key_hash,
+    get_address_b58_from_redeem_script_hash,
+    get_address_from_public_key_hash,
+    get_address_from_redeem_script_hash,
+    get_checksum,
+    get_hash160,
+    get_public_key_bytes_compressed,
+    get_public_key_from_bytes_compressed,
+)
 
 
 def int_to_bytes(number: int, size: int, signed: bool = False) -> bytes:
@@ -48,166 +55,11 @@ def unpack_len(n: int, buf: bytes) -> Tuple[bytes, bytes]:
     return buf[:n], buf[n:]
 
 
-def get_checksum(address_bytes: bytes) -> bytes:
-    """ Calculate double sha256 of address and gets first 4 bytes
-
-        :param address_bytes: address before checksum
-        :param address_bytes: bytes
-
-        :return: checksum of the address
-        :rtype: bytes
-    """
-    return hashlib.sha256(hashlib.sha256(address_bytes).digest()).digest()[:4]
-
-
-def decode_address(address58: str) -> bytes:
-    """ Decode address in base58 to bytes
-
-    :param address58: Wallet address in base58
-    :type address58: string
-
-    :raises InvalidAddress: if address58 is not a valid base58 string or
-                            not a valid address or has invalid checksum
-
-    :return: Address in bytes
-    :rtype: bytes
-    """
-    try:
-        decoded_address = base58.b58decode(address58)
-    except ValueError:
-        # Invalid base58 string
-        raise InvalidAddress('Invalid base58 address')
-    # Validate address size [25 bytes]
-    if len(decoded_address) != 25:
-        raise InvalidAddress('Address size must have 25 bytes')
-    # Validate the checksum
-    address_checksum = decoded_address[-4:]
-    valid_checksum = get_checksum(decoded_address[:-4])
-    if address_checksum != valid_checksum:
-        raise InvalidAddress('Invalid checksum of address')
-    return decoded_address
-
-
-def get_address_b58_from_public_key_hash(public_key_hash: bytes) -> str:
-    """Gets the b58 address from the hash of a public key.
-
-        :param public_key_hash: hash of public key (sha256 and ripemd160)
-        :param public_key_hash: bytes
-
-        :return: address in base 58
-        :rtype: string
-    """
-    address = get_address_from_public_key_hash(public_key_hash)
-    return base58.b58encode(address).decode('utf-8')
-
-
-def get_address_from_public_key_hash(public_key_hash: bytes,
-                                     version_byte: bytes = settings.P2PKH_VERSION_BYTE) -> bytes:
-    """Gets the address in bytes from the public key hash
-
-        :param public_key_hash: hash of public key (sha256 and ripemd160)
-        :param public_key_hash: bytes
-
-        :param version_byte: first byte of address to define the version of this address
-        :param version_byte: bytes
-
-        :return: address in bytes
-        :rtype: bytes
-    """
-    address = b''
-    # Version byte
-    address += version_byte
-    # Pubkey hash
-    address += public_key_hash
-    checksum = get_checksum(address)
-    address += checksum
-    return address
-
-
-def get_address_b58_from_redeem_script_hash(redeem_script_hash: bytes,
-                                            version_byte: bytes = settings.MULTISIG_VERSION_BYTE) -> str:
-    """Gets the b58 address from the hash of the redeem script in multisig.
-
-        :param redeem_script_hash: hash of the redeem script (sha256 and ripemd160)
-        :param redeem_script_hash: bytes
-
-        :return: address in base 58
-        :rtype: string
-    """
-    address = get_address_from_redeem_script_hash(redeem_script_hash, version_byte)
-    return base58.b58encode(address).decode('utf-8')
-
-
-def get_address_from_redeem_script_hash(redeem_script_hash: bytes,
-                                        version_byte: bytes = settings.MULTISIG_VERSION_BYTE) -> bytes:
-    """Gets the address in bytes from the redeem script hash
-
-        :param redeem_script_hash: hash of redeem script (sha256 and ripemd160)
-        :param redeem_script_hash: bytes
-
-        :param version_byte: first byte of address to define the version of this address
-        :param version_byte: bytes
-
-        :return: address in bytes
-        :rtype: bytes
-    """
-    address = b''
-    # Version byte
-    address += version_byte
-    # redeem script hash
-    address += redeem_script_hash
-    checksum = get_checksum(address)
-    address += checksum
-    return address
-
-
 def clean_token_string(string: str) -> str:
     """ Receives the token name/symbol and returns it after some cleanups.
         It sets to uppercase, removes double spaces and spaces at the beginning and end.
     """
     return re.sub(r'\s\s+', ' ', string).strip().upper()
-
-
-def get_public_key_from_bytes_compressed(public_key_bytes: bytes) -> ec.EllipticCurvePublicKey:
-    """Return the cryptography public key from the compressed bytes format."""
-    return ec.EllipticCurvePublicKey.from_encoded_point(ec.SECP256K1(), public_key_bytes)
-
-
-def get_address_b58_from_public_key(public_key: ec.EllipticCurvePublicKey) -> str:
-    """Get the b58 address from a public key."""
-    public_key_bytes = get_public_key_bytes_compressed(public_key)
-    return get_address_b58_from_public_key_bytes(public_key_bytes)
-
-
-def get_address_b58_from_public_key_bytes(public_key_bytes: bytes) -> str:
-    """Get the b58 address from a public key bytes."""
-    public_key_hash = get_hash160(public_key_bytes)
-    return get_address_b58_from_public_key_hash(public_key_hash)
-
-
-def get_public_key_bytes_compressed(public_key: ec.EllipticCurvePublicKey) -> bytes:
-    """Return the bytes of a pubkey in the compressed format."""
-    return public_key.public_bytes(Encoding.X962, PublicFormat.CompressedPoint)
-
-
-try:
-    hashlib.new('ripemd160', b'')
-except Exception:
-    # XXX: the source says "Test-only pure Python RIPEMD160 implementation", however for our case this is acceptable
-    #      for more details see: https://github.com/bitcoin/bitcoin/pull/23716/files which has a copy of the same code
-    import pycoin.contrib.ripemd160  # type: ignore[import-untyped]
-
-    def get_hash160(public_key_bytes: bytes) -> bytes:
-        """The input is hashed twice: first with SHA-256 and then with RIPEMD-160"""
-        key_hash = hashlib.sha256(public_key_bytes)
-        return cast(bytes, pycoin.contrib.ripemd160.ripemd160(key_hash.digest()))
-else:
-    def get_hash160(public_key_bytes: bytes) -> bytes:
-        """The input is hashed twice: first with SHA-256 and then with RIPEMD-160"""
-        key_hash = hashlib.sha256(public_key_bytes)
-        h = hashlib.new('ripemd160')
-        h.update(key_hash.digest())
-        return h.digest()
 
 
 def encode_signed(value: int, *, max_bytes: Union[int, None] = None) -> bytes:
